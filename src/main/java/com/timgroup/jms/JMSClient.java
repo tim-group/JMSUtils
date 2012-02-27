8930eebf4f57ed9a9be6fbe7909be86f25b07381
Tom Anderson <tom.anderson@timgroup.com>
2012-02-27 18:29:00 +0000

    Added sendTextMessage, which reads from stdin

--- src/main/java/com/timgroup/jms/JMSClient.java
+++ src/main/java/com/timgroup/jms/JMSClient.java
@@ -1,5 +1,9 @@
 package com.timgroup.jms;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URI;
 
 import javax.jms.Connection;
@@ -40,6 +44,28 @@ public abstract class JMSClient {
         finally {
             closeQuietly(connection);
         }
+    }
+    
+    public void sendTextMessage(String queueName) throws JMSException {
+        String text;
+        try {
+            text = readFully(System.in);
+        }
+        catch (IOException e) {
+            throw JMSUtil.newJMSException("error reading from standard input", e);
+        }
+        sendShortTextMessage(queueName, text);
+    }
+    
+    private String readFully(InputStream stream) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(stream, "UTF-8"));
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while ((line = in.readLine()) != null) {
+            sb.append(line);
+            sb.append("\n");
+        }
+        return sb.toString();
     }
     
     private void closeQuietly(Connection connection) {
