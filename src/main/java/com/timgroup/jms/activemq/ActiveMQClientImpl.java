29cd79e32d84ea80434e103b830dc2625378f957
Tom Anderson <tom.anderson@timgroup.com>
2012-06-12 16:47:29 +0100

    Remove junk parameters from failover URL (just set wireFormat.maxInactivityDuration on the broker)

--- src/main/java/com/timgroup/jms/activemq/ActiveMQClientImpl.java
+++ src/main/java/com/timgroup/jms/activemq/ActiveMQClientImpl.java
@@ -10,6 +10,8 @@ import javax.jms.QueueConnection;
 import javax.jms.Session;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.timgroup.jms.JMSClient;
 import com.timgroup.jms.JMSUtil;
@@ -17,6 +19,8 @@ import com.timgroup.util.Utils;
 
 public class ActiveMQClientImpl extends JMSClient {
     
+    private static final Logger LOGGER = LoggerFactory.getLogger(ActiveMQClientImpl.class);
+
     public static class Factory implements JMSClient.Factory {
         
         @Override
@@ -44,13 +48,14 @@ public class ActiveMQClientImpl extends JMSClient {
                 sb.append(newTCPURL(inetSocketAddress.getHostName(), port(inetSocketAddress.getPort()), null));
             }
             sb.append(")");
-            sb.append("?randomize=false&jms.prefetchPolicy.all=1&timeout=1000");
+            sb.append("?randomize=false&jms.prefetchPolicy.all=1");
             try {
                 brokerURL = new URI(sb.toString());
             } catch (URISyntaxException e) {
                 throw JMSUtil.newJMSException("all-too plausible error constructing broker URL", e);
             }
         }
+        LOGGER.info("connecting with broker URL {}", brokerURL);
         connectionFactory = new ActiveMQConnectionFactory(brokerURL);
     }
     
